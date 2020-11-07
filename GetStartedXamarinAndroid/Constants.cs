d6b33e8952e2317bb53abd5c7de5d92b1da5c0a6
Gerardo <Villalobos>
2020-11-07 12:30:32 -0600

    First Commit

---
+++ GetStartedXamarinAndroid/Constants.cs
@@ -0,0 +1,10 @@
+﻿
+namespace GetStartedXamarinAndroid
+{
+    public static class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://xamarinhubmtwdm.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=TfH0I3Gmy83vRw5RVFy02vT1ne6Hj+wtr/A3bfRqwYI=";
+        public const string NotificationHubName = "misnotificacioneshub";
+        public const string ChannelId = "micanal";
+    }
+}
---
+++ GetStartedXamarinAndroid/obj/Debug/81/android/src/crc64ec13fd09b39506a7/MyFirebaseIIDService.java
@@ -0,0 +1,47 @@
+package crc64ec13fd09b39506a7;
+
+
+public class MyFirebaseIIDService
+	extends com.google.firebase.iid.FirebaseInstanceIdService
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onTokenRefresh:()V:GetOnTokenRefreshHandler\n" +
+			"";
+		mono.android.Runtime.register ("GetStartedXamarinAndroid.MyFirebaseIIDService, GetStartedXamarinAndroid", MyFirebaseIIDService.class, __md_methods);
+	}
+
+
+	public MyFirebaseIIDService ()
+	{
+		super ();
+		if (getClass () == MyFirebaseIIDService.class)
+			mono.android.TypeManager.Activate ("GetStartedXamarinAndroid.MyFirebaseIIDService, GetStartedXamarinAndroid", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onTokenRefresh ()
+	{
+		n_onTokenRefresh ();
+	}
+
+	private native void n_onTokenRefresh ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
